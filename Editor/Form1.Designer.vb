126462edc742a829656022506fa11bed1bfa173c
Manjul <manjulcr7@gmail.com>
2019-01-25 15:23:57 +0530

    Add project files.

---
+++ Editor/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Exit1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Exit1
+        '
+        Me.Exit1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Exit1.Font = New System.Drawing.Font("Chiller", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Exit1.Location = New System.Drawing.Point(738, 405)
+        Me.Exit1.Name = "Exit1"
+        Me.Exit1.Size = New System.Drawing.Size(146, 53)
+        Me.Exit1.TabIndex = 0
+        Me.Exit1.Text = "E&xit" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.Exit1.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.Control
+        Me.TextBox1.Location = New System.Drawing.Point(39, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(697, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "This is a Text-Box."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.BackgroundImage = Global.WindowsApp1.My.Resources.Resources.g
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(896, 470)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Exit1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Exit1 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Editor/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Exit1_Click(sender As Object, e As EventArgs) Handles Exit1.Click
+        End
+    End Sub
+End Class
